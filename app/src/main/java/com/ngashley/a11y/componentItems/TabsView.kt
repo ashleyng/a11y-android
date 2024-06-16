2015c2cc4d5d15139413b8ab46f53fe51e67ee62
Ashley Ng <ashleyng@users.noreply.github.com>
2024-06-16 13:31:24 -0500

    Update some strings and fix tab content descriptions

--- app/src/main/java/com/ngashley/a11y/componentItems/TabsView.kt
+++ app/src/main/java/com/ngashley/a11y/componentItems/TabsView.kt
@@ -1,5 +1,6 @@
 package com.ngashley.a11y.componentItems
 
+import android.icu.text.NumberFormat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +20,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ngashley.a11y.R
@@ -29,9 +32,21 @@ fun TabsView(modifier: Modifier = Modifier) {
     var state1 by remember { mutableStateOf(0) }
     var state2 by remember { mutableStateOf(0) }
     var state3 by remember { mutableStateOf(0) }
-    val titles = listOf("Tab 1", "Tab 2", "Tab 3 with lots of text")
-    val alotOfTitles = listOf("Tab 1", "Tab 2", "Tab 3 with lots of text", "Tab 4", "Tab 5", "Tab 6", "Tab 7")
-
+    val numberFormatter = NumberFormat.getIntegerInstance()
+    val titles = listOf(
+        stringResource(id = R.string.tab_number, numberFormatter.format(1)),
+        stringResource(id = R.string.tab_number, numberFormatter.format(2)),
+        stringResource(id = R.string.long_tab_number, numberFormatter.format(3))
+    )
+    val alotOfTitles = listOf(
+        stringResource(id = R.string.tab_number, numberFormatter.format(1)),
+        stringResource(id = R.string.tab_number, numberFormatter.format(2)),
+        stringResource(id = R.string.long_tab_number, numberFormatter.format(3)),
+        stringResource(id = R.string.tab_number, numberFormatter.format(4)),
+        stringResource(id = R.string.tab_number, numberFormatter.format(5)),
+        stringResource(id = R.string.tab_number, numberFormatter.format(6)),
+        stringResource(id = R.string.tab_number, numberFormatter.format(7))
+    )
 
     Column(modifier = modifier
         .verticalScroll(rememberScrollState())
@@ -39,27 +54,37 @@ fun TabsView(modifier: Modifier = Modifier) {
         .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(32.dp)) {
 
+        Text(text = stringResource(id = R.string.tab_semantics))
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Text(text = stringResource(id = R.string.primary_tab))
+            val titleString = stringResource(id = R.string.primary_tab)
+            Text(text = titleString)
             PrimaryTabRow(selectedTabIndex = state1) {
                 titles.forEachIndexed { index, title ->
                     Tab(
+                        modifier = Modifier.semantics { 
+                            contentDescription = "$titleString, $title"
+                        },
                         selected = state1 == index,
                         onClick = { state1 = index },
                         text = { Text(text = title) }
                     )
                 }
             }
+            
         }
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Text(text = stringResource(id = R.string.secondary_tab))
+            val titleString = stringResource(id = R.string.secondary_tab)
+            Text(text = titleString)
             SecondaryTabRow(selectedTabIndex = state2) {
                 titles.forEachIndexed { index, title ->
                     Tab(
+                        modifier = Modifier.semantics { 
+                            contentDescription = "$titleString, $title"
+                        },
                         selected = (index == state2),
                         onClick = { state2 = index },
                         text = { Text(title) }
@@ -70,10 +95,14 @@ fun TabsView(modifier: Modifier = Modifier) {
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Text(text = stringResource(id = R.string.scrollable_tab))
+            val titleString = stringResource(id = R.string.scrollable_tab)
+            Text(text = titleString)
             ScrollableTabRow(selectedTabIndex = state3) {
                 alotOfTitles.forEachIndexed { index, title ->
                     Tab(
+                        modifier = Modifier.semantics {
+                            contentDescription = "$titleString, $title"
+                        },
                         selected = (index == state3),
                         onClick = { state3 = index },
                         text = { Text(title) }
--- app/src/main/java/com/ngashley/a11y/main/AboutAppView.kt
+++ app/src/main/java/com/ngashley/a11y/main/AboutAppView.kt
@@ -4,17 +4,24 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.ngashley.a11y.R
 
 @Composable
 fun AboutAppView() {
+    val annotatedStringTag = "github_tag"
+
     Column(
         modifier = Modifier
             .padding(8.dp)
@@ -30,6 +37,20 @@ fun AboutAppView() {
         Text(text = stringResource(id = R.string.about_app2))
         Text(text = stringResource(id = R.string.about_app3))
         Text(text = stringResource(id = R.string.about_app4))
+        val annotatedString = buildAnnotatedString {
+            val string = String.format(stringResource(id = R.string.app_on_github), "A11y Example", "GitHub")
+            pushStringAnnotation(tag = annotatedStringTag, annotation = "https://github.com/ashleyng/a11y-android")
+            withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                append(string)
+            }
+        }
+        val uriHandler = LocalUriHandler.current
+        ClickableText(
+            text = annotatedString) { offset ->
+            annotatedString.getStringAnnotations(tag = annotatedStringTag, start = offset, end = offset).firstOrNull()?.let {
+                uriHandler.openUri(it.item)
+            }
+        }
     }
 }
 
